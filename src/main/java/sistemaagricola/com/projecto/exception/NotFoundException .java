bdb526143d103b2df3c82e3cd58ff1b2c9172bd5
abelmandjr <abelmandjr@gmail.com>
2025-11-06 12:35:45 +0200

    Excepcao not found criqda

---
+++ src/main/java/sistemaagricola/com/projecto/exception/NotFoundException .java
@@ -0,0 +1,5 @@
+// src/main/java/com/agro/iot/exception/NotFoundException.java
+package com.agro.iot.exception;
+public class NotFoundException extends RuntimeException {
+  public NotFoundException(String msg){ super(msg); }
+}
