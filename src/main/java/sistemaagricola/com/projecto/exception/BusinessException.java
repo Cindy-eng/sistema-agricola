e11519731c1f623893f3384b2ba77c122012e882
abelmandjr <abelmandjr@gmail.com>
2025-11-06 12:40:22 +0200

    Classe de Excepcao de Classe de excepcao do negocio

---
+++ src/main/java/sistemaagricola/com/projecto/exception/BusinessException.java
@@ -0,0 +1,5 @@
+// src/main/java/com/agro/iot/exception/BusinessException.java
+package com.agro.iot.exception;
+public class BusinessException extends RuntimeException {
+  public BusinessException(String msg){ super(msg); }
+}
