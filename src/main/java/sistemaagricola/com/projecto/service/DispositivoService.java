f96b914218576bb15570f7866fcf5358e6fb0cd0
antonio-zandamela <tonymanasses@gmail.com>
2025-11-10 14:45:54 +0200

    Serviço Dispositivo

---
+++ src/main/java/sistemaagricola/com/projecto/service/DispositivoService.java
@@ -0,0 +1,12 @@
+package com.agro.iot.service;
+import com.agro.iot.model.Dispositivo; import org.springframework.data.domain.*;
+import java.util.*;
+
+public interface DispositivoService {
+  Dispositivo criar(Dispositivo d);
+  Optional<Dispositivo> obterPorDeviceKey(String deviceKey);
+  Dispositivo obter(Long id);
+  Page<Dispositivo> listarPorParcela(Long parcelaId, Pageable pg);
+  Dispositivo alterarEstado(Long id, Dispositivo.Estado estado);
+  List<Dispositivo> listarTodos();
+}
