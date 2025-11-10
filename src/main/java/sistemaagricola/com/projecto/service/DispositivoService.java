9214996453da61d05920ac155fb7592af0b4062f
antonio-zandamela <tonymanasses@gmail.com>
2025-11-10 16:26:23 +0200

    Servico Dispositivo Resolucao Conflito

--- src/main/java/sistemaagricola/com/projecto/service/DispositivoService.java
+++ src/main/java/sistemaagricola/com/projecto/service/DispositivoService.java
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 package sistemaagricola.com.projecto.service;
 
-// service/DispositivoService.java
-import sistemaagricola.com.projecto.models.Dispositivo; import org.springframework.data.domain.*;
+import sistemaagricola.com.projecto.models.Dispositivo;
+import org.springframework.data.domain.*;
 import java.util.*;
 
 public interface DispositivoService {
-=======
-    package sistemaagricola.com.projecto.service;
-    import sistemaagricola.com.projecto.models.Dispositivo;
-    import org.springframework.data.domain.*;
-    import java.util.*;
-
-    public interface DispositivoService {
->>>>>>> origin/master
     Dispositivo criar(Dispositivo d);
     Optional<Dispositivo> obterPorDeviceKey(String deviceKey);
     Dispositivo obter(Long id);
     Page<Dispositivo> listarPorParcela(Long parcelaId, Pageable pg);
     Dispositivo alterarEstado(Long id, Dispositivo.Estado estado);
     List<Dispositivo> listarTodos();
-<<<<<<< HEAD
 }
-=======
-    }
->>>>>>> origin/master
